8645b4af840bb71f6756c76f4660b0efe440d78d
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2021-06-13 20:55:08 -0300

    Configurando menu, item de menu e toolbar

--- app/src/main/java/br/com/denise/castro/notas/MainActivity.kt
+++ app/src/main/java/br/com/denise/castro/notas/MainActivity.kt
@@ -2,10 +2,27 @@ package br.com.denise.castro.notas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_principal, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item!!.itemId == R.id.action_adicionar) {
+            //TODO: Chamar tela para adicionar nova nota
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
